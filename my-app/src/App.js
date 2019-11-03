4bd8935c6b1bfc9ac224a1819e5a29577d5556f0
Miguel <Garzarobm@utexas.edu>
2019-11-02 19:12:10 -0500

    my garbage

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,8 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Button } from "reactstrap";
+import { Dropdown } from "reactstrap";
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <Button color="danger">Danger!</Button>
+        <Dropdown>hello</Dropdown>
         <a
           className="App-link"
           href="https://reactjs.org"
