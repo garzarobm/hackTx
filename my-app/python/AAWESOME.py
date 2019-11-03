79d2694fe1192bf4232f29f3ff2e30a80d90f371
Errol Williams II <Errolwilliamsii@gmail.com>
2019-11-03 03:39:51 -0600

    connected darksky api

--- my-app/python/AAWESOME.py
+++ my-app/python/AAWESOME.py
@@ -1,11 +1,16 @@
 from flask import Flask, request
+from darksky import forecast
+from datetime import datetime
+from datetime import timedelta
 import json
+import numpy as np
 import urllib.parse
 import urllib.request
 
 app = Flask(__name__)
 
-url = 'http://antoniodidnothingwrong-hacktx.herokuapp.com/flights?'
+aa_url = 'http://antoniodidnothingwrong-hacktx.herokuapp.com/flights?'
+key = '605880dc58036d59e4a24c7f244a7a09'
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -16,7 +21,10 @@ def result():
         date = request.args.get('date', None)
 
         if date is None:
-            date = '2020-01-01'
+            year = 2020
+            month = 1
+            day = 1
+            date = f'{year}-{month:02d}-{day:02d}'
 
         values = {'date': date}
 
@@ -26,25 +34,54 @@ def result():
         if destination is not None:
             values['destination'] = destination
 
-        data = urllib.parse.urlencode(values)
-        req = urllib.request.Request(url + data)
+        aa_data = urllib.parse.urlencode(values)
+        req = urllib.request.Request(aa_url + aa_data)
         with urllib.request.urlopen(req) as response:
             the_page = response.read().decode('utf-8')
             json_responses = json.loads(the_page)
-            for json_response in json_responses:
+            for json_response in json_responses[:1]:
                 origin_latitude = json_response['origin']['location']['latitude']
                 origin_longitude = json_response['origin']['location']['longitude']
                 destination_latitude = json_response['destination']['location']['latitude']
                 destination_longitude = json_response['destination']['location']['longitude']
-                duration = json_response['duration']['hours']*60 + json_response['duration']['minutes']
+                duration = json_response['duration']['hours'] * 60 + json_response['duration']['minutes']
 
                 flight_number = json_response['flightNumber']
                 print(f'flight_num: {flight_number}')
+
+                """
                 print(f'Origin lat:{origin_latitude}')
                 print(f'Origin long:{origin_longitude}')
                 print(f'Destination lat:{destination_latitude}')
                 print(f'Destination long:{destination_longitude}')
                 print(f'Duration: {duration}')
+                """
+
+                hour = np.random.randint(0, 24)
+                minute = np.random.randint(0, 60)
+                dt = datetime(year, month, day, hour, minute, 0)
+                # date_str = dt_obj.strftime("%Y-%m-%d %H:%M:%S")
+                time_segments = duration // 15
+
+                times = [dt + timedelta(minutes=i * 15) for i in range(time_segments + 1)]
+                latitude_rate = (destination_latitude - origin_latitude) / time_segments
+                longitude_rate = (destination_longitude - origin_longitude) / time_segments
+                latitude_points = [origin_latitude + latitude_rate * i for i in range(time_segments + 1)]
+                longitude_points = [origin_longitude + longitude_rate * i for i in range(time_segments + 1)]
+
+                for i in range(len(longitude_points)):
+                    latitude = latitude_points[i]
+                    longitude = longitude_points[i]
+                    cast = forecast(key, latitude, longitude, time=times[i].isoformat())
+                    print(i)
+                    print(dir(cast))
+                    if 'icon' in cast['currently']:
+                        icon = cast['currently']['icon']
+                        print(icon)
+
+                print(latitude_points)
+                print(longitude_points)
+                print(times)
                 print()
 
 
